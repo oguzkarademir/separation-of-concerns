73fedb313d5fdce0dd46cc1d02c6c973e32c1184
oguz <oguzkarademir26@gmail.com>
2020-07-26 15:02:26 +0200

    integrate exercises are completed

--- isolate/03-html-templates/exercises/write-the-template-1.js
+++ isolate/03-html-templates/exercises/write-the-template-1.js
@@ -7,7 +7,9 @@
  * @returns {string} rendered header tag
  */
 const renderHeader = (level, text) => {
-  return _;
+  return (
+    `<h${level}>${text}</h${level}>`
+  );
 };
 
 const happyH1 = renderHeader(1, ':)');
@@ -22,6 +24,6 @@ console.assert(sadH2 === '<h2>:(</h2>',
 
 const divEl = document.createElement('div');
 
-divEl.innerHTML = happyH2;
-divEl.innerHTML += sadH1;
+divEl.innerHTML = happyH1;
+divEl.innerHTML += sadH2;
 console.log(divEl);
